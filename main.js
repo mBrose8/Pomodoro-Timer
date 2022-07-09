0943b79603fe26827cc5dc664836e3036f1cdffe
Maurício Martins <martinzbrose8@gmail.com>
2022-07-08 22:00:12 -0300

    Pomodoro Timer

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const stop = document.getElementById("pausar");
 const reset = document.getElementById("resetar");
 
 const workMinutes = document.getElementById("workMinutes");
-const work  Seconds = document.getElementById("workSeconds");
+const workSeconds = document.getElementById("workSeconds");
 
 const breakMinutes = document.getElementById("breakMinutes");
 const breakSeconds = document.getElementById("breakSeconds");
